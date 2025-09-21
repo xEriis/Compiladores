5e4b7cde376b982aeb14b497c5339d969db77d0d
xEriis <Eris@ciencias.unam.mx>
2025-09-20 23:06:42 -0600

    Avance con listas (sin usar sets), correcciones necesarias

--- Proyecto/app/AFN.hs
+++ Proyecto/app/AFN.hs
@@ -1,17 +1,14 @@
 module AFN
 
 where
--- Buscando en internet, set en haskell permite operaciones con conjuntos
--- (ya que el método de AFN a AFD es con operaciones de conjuntos)
-import qualified Data.Set as Set
-import Data.Set (Set, fromList, toList, unions, map) --Set es quien vamos a usar 
-
 -- Elementos del autómata finito no determinista
+import Data.List (nub,sort)
+
 type Estado = Int
 type Simbolo = Char
-type EstadosAlcazables = Set Estado -- Conjunto de estados alcanzables a partir de un estado
+type EstadosAlcazables = [Estado] -- Conjunto de estados alcanzables a partir de un estado
 
-type DeltaND = Estado -> Simbolo -> EstadosAlcazables --la función de transición ahora nos puede llevar a una lista de estados
+type DeltaND = Estado -> Simbolo -> [Estado] --la función de transición ahora nos puede llevar a una lista de estados
 data AFN = AFN {
     estadosAFN :: [Estado],
     alfabetoAFN :: [Simbolo], --Aunque el alfabeto será el mismo que el AFD
@@ -24,21 +21,22 @@ data AFN = AFN {
 -- Quintupla del estado finito determinista
 -- Usaremos los conjuntos en crudo para determinar a donde se va, es decir, el conjunto de estados alcanzables
 -- se volverá un estado nuevo posteriormente en el AFN
-type DeltaD = EstadosAlcazables -> Simbolo -> EstadosAlcazables --Función de transición de autómata determinista
 data AFD = AFD {
-    estadosAFD :: [EstadosAlcazables], --Lista de estados
+    estadosAFD :: [[Estado]], --Lista de estados
     alfabetoAFD :: [Simbolo], --Simbolos 
-    deltaAFD :: DeltaD, --Ahora vamos de conjuntos de estados con un simbolo a otro conjunto de estados
-    inicialAFD :: EstadosAlcazables, 
-    finalesAFD :: [EstadosAlcazables]
+    deltaAFD :: [Estado] -> Simbolo -> [Estado],
+    inicialAFD :: [Estado], 
+    finalesAFD :: [[Estado]]
 }
 
-nuevosEstadosFinales :: AFN -> EstadosAlcazables -> Bool -- Si un estado final está en alguno de los subconjuntos
-nuevosEstadosFinales afn estados = any (`elem` finalesAFN afn) (toList estados)
+nuevosEstadosFinales :: AFN -> [Estado] -> Bool -- Si un estado final está en alguno de los subconjuntos
+nuevosEstadosFinales afn estados = any (`elem` finalesAFN afn) estados
 
 -- Partimos de unir los estados alcanzables para crear nuevos (estos funcionan como nuestros nuevos estados)
-subconjuntosAFN :: AFN -> EstadosAlcazables -> Simbolo -> EstadosAlcazables
-subconjuntosAFN afn estados simbolo = unions [deltaAFN afn estado simbolo | estado <- toList estados]
+subconjuntosAFN :: AFN -> [Estado] -> Simbolo -> [Estado]
+-- nub elimina duplicados, mientras que sort como se intuye, ordena. (En haskell [0,1] es distinto de [1,0], tiene sentido, por lo que al ordenarlos solo tenemos una opción)
+-- La unica mala "desición" que tenemos esque nub es O(n)^2, por lo cual si el profe nos pide optimizar conviene mejor set o monedas
+subconjuntosAFN afn estados simbolo = nub . sort . concat $ [deltaAFN afn e simbolo | e <- estados]
 
 {-|
 Función la cual hace la conversión del autómata finito no determinista a un autómata finito determinista
@@ -46,24 +44,24 @@ recibe un AFN y lo transforma en un AFD, cada conjunto de estados representa un 
 -}
 trans_afn_a_afd :: AFN -> AFD
 trans_afn_a_afd afn = 
-    let estadoinicial = fromList [inicialAFN afn] --iniciamos del estado inicial del autómata finito no determinista
+    let estadoinicial = [inicialAFN afn] --iniciamos del estado inicial del autómata finito no determinista
         -- Los nuevos que iremos obteniendo son aquellos que nos llevan de uno o más estados alcanzables hacia más estados alcazables
-        estadosNuevos :: [EstadosAlcazables] -> [EstadosAlcazables] -> [EstadosAlcazables]
+        estadosNuevos :: [[Estado]] -> [[Estado]] -> [[Estado]]
         estadosNuevos revisados [] = revisados
         estadosNuevos revisados (actual:siguiente) = 
             -- los estados se procesan estando en el actual con el símbolo correspondiente en el afn
             let estados = [subconjuntosAFN afn actual simbolo | simbolo <- alfabetoAFN afn]
-                -- estados no revisados con lambda, si su elemento no está en revisado y si 
-                noRevisado = filter (\nr -> not (nr `elem` revisados) && not (null nr)) estados
+                -- Eliminación de repetidos y ordenamiento para evitar listas distintas
+                ordenamientoEstados = filter (not . null) (map (nub . sort) estados)
+                -- estados no revisados asegurados que no están repetidos y no nulos
+                noRevisado = filter (`notElem` (revisados ++ siguiente)) ordenamientoEstados
                 -- Listas de los estados que ya revisamos en el AFN y de aquellos que no.
-                estadosRecorridos = revisados ++ [actual] 
+                estadosRecorridos = (revisados ++ [actual]) 
                 noRecorridos = siguiente ++ noRevisado
             in estadosNuevos estadosRecorridos noRecorridos
 
         -- Partes resultantes de la autómata finita determinista
-        --función de transición delta (sin normalizar, vamos de un conjunto de estados a mutiples conjuntos de estados)
-        delta :: EstadosAlcazables -> Simbolo -> EstadosAlcazables
-        delta cjtoEstados simbolo = subconjuntosAFN afn cjtoEstados simbolo
+        delta estados simbolo = subconjuntosAFN afn estados simbolo 
         estadosNuevosAFD = estadosNuevos [] [estadoinicial]
         estadosFinales = filter(nuevosEstadosFinales afn) estadosNuevosAFD
 
--- Proyecto/app/Main.hs
+++ Proyecto/app/Main.hs
@@ -1,29 +1,28 @@
-module Main (main) where
+    module Main (main) where
 
-import qualified Data.Set as Set
-import AFD
-import AFN
+    import AFD
+    import AFN
 
-deltaEj :: Estado -> Simbolo -> EstadosAlcazables
-deltaEj 0 'a' = Set.fromList [0,1]
-deltaEj 1 'a' = Set.empty
-deltaEj _ _   = Set.empty
+    deltaEj :: Estado -> Simbolo -> [Estado]
+    deltaEj 0 'a' = [0,1]
+    deltaEj 1 'a' = []
+    deltaEj _ _   = []
 
-afnEj :: AFN
-afnEj = AFN {
-    estadosAFN = [0,1],
-    alfabetoAFN = ['a'],
-    deltaAFN = deltaEj,
-    inicialAFN = 0,
-    finalesAFN = [1]
-}
+    afnEj :: AFN
+    afnEj = AFN {
+        estadosAFN = [0,1],
+        alfabetoAFN = ['a'],
+        deltaAFN = deltaEj,
+        inicialAFN = 0,
+        finalesAFN = [1]
+    }
 
-main :: IO ()
-main = do
-    -- Prueba sencilla para AFD
-    -- print (acepta "1100" afdPar)
-    let afd = trans_afn_a_afd afnEj
-    putStrLn "Estados del AFD:"
-    print (estadosAFD afd)
-    putStrLn "Finales del AFD:"
-    print (finalesAFD afd)
+    main :: IO ()
+    main = do
+        -- Prueba sencilla para AFD
+        -- print (acepta "1100" afdPar)
+        let afd = trans_afn_a_afd afnEj
+        putStrLn "Estados del AFD:"
+        print (estadosAFD afd)
+        putStrLn "Finales del AFD:"
+        print (finalesAFD afd)
