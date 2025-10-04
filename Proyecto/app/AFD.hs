8fe8550f248d392c6c325596eb2bcd4d18b9db8d
moka <areumm@proton.me>
2025-10-04 17:05:50 -0600

    Algoritmo de minimización funcional

--- Proyecto/app/AFD.hs
+++ Proyecto/app/AFD.hs
@@ -7,8 +7,21 @@ module AFD
 
 where
 
-import Data.List (elemIndex)
+import Data.List (elemIndex, nub, intersect, (\\), sort, isInfixOf)
+-- Se tiene que importar así porque si no, hay un conflicto con el import de 
+-- Data.List ya que tienen funciones llamadas igual
+import qualified Data.Set as Set
 import Data.Matrix
+
+-- Función auxiliar para mejorar la complejidad de quitar elementos dublicados
+-- en una lista, lo convierte a un conjunto para usar un arbol
+nubSet :: Ord a => [a] -> [a]
+nubSet = Set.toList . Set.fromList
+
+-- Función auxiliar para mejorar la complejidad de ordenar una lista al convertilo
+-- a un conjunto, es decir, usa árboles
+sortSet :: Ord a => [a] -> [a]
+sortSet = Set.toAscList . Set.fromList
 
 type Trans_afd = (String, Char, String)
 data AFD = AFD {
@@ -41,22 +54,114 @@ checaTransicion q s (x:xs)
 acepta :: String -> AFD -> Bool
 acepta s (AFD _ _ d q0 f) = transita q0 s d `elem` f
 
--- Todavía haciendose, solo regresa un [[String]] para motivos de debuggin
-minimiza :: AFD -> [[String]]
-minimiza (AFD q a d i f) = estadosEquivalentes q (minimizaAux (AFD q a d i f) (matrix lq lq $ \(i,j) -> -1))
+eliminaInalcanzables :: AFD -> AFD
+eliminaInalcanzables (AFD q a d i f) = (AFD alcanzables a (eliminaTransiciones d noAlcanzables) i (f \\ noAlcanzables))
+                        where alcanzables = getAlcanzables [i] d
+                              noAlcanzables = q \\ alcanzables 
+
+eliminaTransiciones :: [Trans_afd] -> [String] -> [Trans_afd]
+eliminaTransiciones d s = [ (qb, t, qn) | (qb, t, qn) <- d, not (qb `elem` s || qn `elem` s)]
+
+getAlcanzables :: [String] -> [Trans_afd] -> [String]
+getAlcanzables q d
+  | null ep || esSubLista epn qp = q
+  | otherwise = getAlcanzables (nub (qp ++ epn)) d
+  where ep = estadosPosiblesLista q d
+        epn = nub ep
+        qp = nub q
+
+esSubLista :: Eq a => [a] -> [a] -> Bool
+esSubLista xs ys = all (\x -> elem x ys) xs
+
+estadosPosiblesLista :: [String] -> [Trans_afd] -> [String]
+estadosPosiblesLista [] d = []
+estadosPosiblesLista (q:qs) d = [q] ++ qp ++ estadosPosiblesLista qs d
+  where qp = estadosPosibles q d
+
+estadosPosibles :: String -> [Trans_afd] -> [String]
+estadosPosibles q [] = []
+estadosPosibles q (t:ts) 
+  | q0 == q = [qn] ++ (estadosPosibles q ts)
+  | otherwise = estadosPosibles q ts
+  where (q0, ms, qn) = t
+
+-- Todavía haciendose, solo regresa un [[String]] para motivos de debugging
+minimiza :: AFD -> AFD
+minimiza (AFD q a d i f) = (AFD (minimizaEstados nq eqs) na (nub (minimizaTransiciones nd [] eqs)) (minimizaInicial ni eqs) (nub (minimizaFinales nf eqs)))
   where lq = length q
+        (AFD nq na nd ni nf) = eliminaInalcanzables (AFD q a d i f)
+        nlq = length nq
+        eq = estadosEquivalentes nq (minimizaAux (AFD nq na nd ni nf) (matrix nlq nlq $ \(i,j) -> -1))
+        equ = uneEstados eq eq
+        eqs = [concat s | s <- equ]
+
 
 -- Función auxiliar para minimizar un AFD 
 -- Recibe un AFD, una matriz que representa nuestra tabla y regresa "por ahora"
 -- una tabla
 minimizaAux :: AFD -> Matrix Int -> Matrix Int
-minimizaAux a m = operaTabla2 a m (operaTabla1 a m) 
+minimizaAux a m = (operaTabla2 a m (operaTabla1 a m))
+
+minimizaInicial :: String -> [String] -> String
+minimizaInicial i eq = if null ni then i else ni
+                     where ni = getString i eq
+
+minimizaEstados :: [String] -> [String] -> [String]
+minimizaEstados [] nq = nq
+minimizaEstados (q:qs) nq 
+  | contieneString q nq = (minimizaEstados qs nq)
+  | otherwise = minimizaEstados qs ([q] ++ nq)
+
+minimizaFinales :: [String] -> [String] -> [String]
+minimizaFinales [] nq = []
+minimizaFinales (q:qs) nq 
+  | null ns = [q] ++ (minimizaFinales qs nq)
+  | otherwise = [ns] ++ minimizaFinales qs nq
+  where ns = getString q nq
+
+
+minimizaTransiciones :: [Trans_afd] -> [Trans_afd] -> [String] -> [Trans_afd]
+minimizaTransiciones [] nd _ = nd
+minimizaTransiciones (d:ds) nd s = minimizaTransiciones ds (nd ++ [(qbn, dt, qnn)]) s
+  where (qb, dt, qn) = d
+        subB = getString qb s
+        subN = getString qn s
+        qbn = if null subB then qb else subB 
+        qnn = if null subN then qn else subN
+
+getString :: String -> [String] -> String
+getString s1 s2 = case [x | x <- s2, s1 `isInfixOf` x] of 
+                (x:_) -> x 
+                [] -> ""
+
+contieneString :: String -> [String] -> Bool 
+contieneString s1 ls = any (s1 `isInfixOf`) ls
+
+
+uneEstados:: [[String]] -> [[String]] -> [[String]]
+uneEstados [] _ = []
+uneEstados (q:qs) eq = nubSet ([nuevoQ] ++ uneEstados qs eq)
+                  where nuevoQ = sortSet (nubSet (uneListasIntersectadas q eq))
+--uneTransiciones (sp:sps) (d:ds)
+
+uneListasIntersectadas :: [String] -> [[String]] -> [String]
+uneListasIntersectadas a [] = []
+uneListasIntersectadas a (x:xs)
+  | null i = uneListasIntersectadas a xs
+  | otherwise = na ++ nubSet (uneListasIntersectadas na xs)
+  where i = a `intersect` x
+        na = nubSet (a ++ (x \\ a))
 
 -- El primer paso del algoritmo de minimización provisto por Diego
 -- Checa si alguno de los dos estados correspondientes a una casilla 
 -- son iguales o no, si sí se marca con 1, si no se marca con un 0
 -- Recibe un AFD una matriz de enteros que representa la tabla
 -- Regresa una matriz de enteros modificada por el primer paso
+-- Nota: en las matrices de Haskell los indices comienzan en (1,1)
+-- así que para obtener los estados correspondientes en la lista 
+-- de estados se usa j-1 e i sin que i se salga del length de la 
+-- lista de estados, esto para hacer las modificaciones de manera correcta
+-- y acorde a la tabla de equivalencias del algoritmo visto en clase
 operaTabla1 :: AFD -> Matrix Int -> Matrix Int
 operaTabla1 (AFD q _ _ _ f) table = mapPos(\(i,j) x -> if (i < length q) && (condicion1 f (q !! (j-1)) (q !! (i))) && j <= i then 1 else 0) table
 
@@ -106,7 +211,7 @@ checaCasilla q j i m
 -- de minimización visto en clase, funciona obtiendo todos las posiciones
 -- de la matriz en una lista de posiciones, luego las filtra asegurandose
 -- que se quede con las que quedaron en 0 y además sea del triangulo
--- derecho de la matriz (i.e. j <= i) y que i < length q por como obtenemos
+-- izquierdo de la matriz (i.e. j <= i) y que i < length q por como obtenemos
 -- los estados apropiados dado el algoritmo, por último solo obtenemos los 
 -- estados correspondientes dadas las posiciones encontradas.
 estadosEquivalentes :: [String] -> Matrix Int -> [[String]]
@@ -119,8 +224,8 @@ estadosEquivalentes q m =
       ) listaCoordenadas
   in map (\(i, j) -> [q !! (j-1), q !! i]) equivalentes
 
-automata3States :: AFD
-automata3States = AFD {
+a1 :: AFD
+a1 = AFD {
     estadosD = ["A", "B", "C"],
     alfabetoD = ['0', '1'],
     transicionesD = [
@@ -182,6 +287,38 @@ a3 = AFD {
     ],
     inicialD = "q0",
     finalesD = ["qAA", "qAB", "qBA", "qBB"]
+}
+
+au :: AFD
+au = AFD {
+    estadosD = ["q0", "q1", "q2", "q3", "qUNR_A", "qUNR_B", "qUNR_C"],
+    alfabetoD = ['a', 'b'],
+    transicionesD = [
+        -- REACHABLE COMPONENT (Language: a...a, |w| >= 3)
+        ("q0", 'a', "q1"),
+        ("q0", 'b', "q3"), -- Trap on 'b' because it must start with 'a'
+
+        ("q1", 'a', "q2"),
+        ("q1", 'b', "q2"),
+
+        ("q2", 'a', "q3"), -- Final condition: Saw final 'a'
+        ("q2", 'b', "q2"), -- Loop until final 'a' is seen
+
+        ("q3", 'a', "q3"), -- Loop: Stays final on 'a'
+        ("q3", 'b', "q2"), -- Switches back to intermediate state on 'b'
+
+        -- UNREACHABLE COMPONENT 1 (Simple Dead State)
+        ("qUNR_A", 'a', "qUNR_A"),
+        ("qUNR_A", 'b', "qUNR_A"),
+
+        -- UNREACHABLE COMPONENT 2 (Two interconnected states)
+        ("qUNR_B", 'a', "qUNR_C"),
+        ("qUNR_B", 'b', "qUNR_B"),
+        ("qUNR_C", 'a', "qUNR_B"),
+        ("qUNR_C", 'b', "qUNR_C")
+    ],
+    inicialD = "q0",
+    finalesD = ["q3", "qUNR_B"]
 }
 --marcaCasilla :: Matrix Int -> Matrix Int
 
